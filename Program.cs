ab0a66fbf432742e40c06f880698ad0de3e3a692
Lenovo <Lenovo@LAPTOP-ON21VDT4>
2023-09-28 16:27:10 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MatchingAngleBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Strings s = new Strings();
+    
+            string testString = "<><><><><ashdjhasjdhjakshdjkhaskjdhjkashjk::!!!dhjkashdjkasd><><><><div></div>";
+
+            Console.WriteLine("String to test? ");
+            testString = Console.ReadLine();
+
+            string output = s.GetAngleBrackets(testString, "");
+            //Console.WriteLine(output);
+            //Console.WriteLine("Length={0}; IsPairable={1}", output.Length, s.IsPairable(output));
+            Console.WriteLine("");
+            Console.WriteLine("Has Matched Angle Brackets? {0}", s.AngleBracketsMatched(s.GetPairedStrings(output, s.IsPairable(output))));
+        }
+    }
+}
---
+++ Strings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MatchingAngleBrackets
+{
+    public class Strings
+    {
+        const string regexp1 = @"[^a-zA-Z\d\s:<>]";
+        const string regexp2 = @"[a-zA-Z\d\s:]";
+
+        public string GetAngleBrackets(string srcString, string chrReplace = "")
+        {
+            Regex regExp1 = new Regex(regexp1);
+            Regex regExp2 = new Regex(regexp2);
+            return regExp2.Replace(regExp1.Replace(srcString, chrReplace), chrReplace);
+        }
+
+        public bool IsPairable(string srcString)
+        {
+            return (srcString.Length % 2) == 0;
+        }
+
+        public List<string> GetPairedStrings(string srcString, bool isPairable)
+        {
+            var result = new List<string>();
+
+            if (isPairable) {
+                for (int i = 0; i < srcString.Length; i++)
+                {
+                    result.Add(srcString.Substring(i, 2));
+                    i++;
+                }
+            } else result.Add(srcString);
+
+            return result;
+        }
+
+        public bool AngleBracketsMatched(List<string> l)
+        {
+            return l.All(i => i.Equals("<>"));
+        }
+    }
+}
